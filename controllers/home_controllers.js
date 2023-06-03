c69d7cc0305296dd16f3abe8dafe9ce374a266e4
AnggaChelsea <v-angga.lesmana@anteraja.id>
2023-06-04 00:37:56 +0700

    add ejs logic

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,8 +1,10 @@
-// const axios = require('axios')
-const axios = require('axios')
+
+const axios = require('axios');
 class HomeControllers {
     static async getHome(req, res) {
         try{
+            const resData = await axios.get('https://agadevin-e4ac9-default-rtdb.firebaseio.com/add-project.json')
+            const resultData = resData.data
             const title = 'AgaDiDevin'
             const day = new Date()
             let getDay = day.getDay()
@@ -25,8 +27,12 @@ class HomeControllers {
                 default:
                     console.log('wow ')
             }
-    
-            res.render('index', {data: title, date: currday})
+            console.log(resultData)
+            const arr = []
+            for(key in resultData ){
+                arr.push(resultData[key])
+            }
+            res.render('index', {data: title, date: currday, dataCard: resultData})
         }
         catch{
             res.status(500).send('<h1>Error Server 500</h1>')
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
-const rootDir = require('../helpers/path')
 
 router.get('/admin', (req, res) => {
-    const today = new Date()
-    const yesterday = new Date(today.setDate(today.getDay() - 1))
-    console.log(yesterday)
-    console.log(today)
-    res.render('admin/admin',{day: today })
+    res.render('template/index.html', {
+        title: 'Admin Page'
+    })
 })
 
 

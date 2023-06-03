682ffa26f8c78cf0e5df39b2a9447601637e1277
AnggaChelsea <v-angga.lesmana@anteraja.id>
2023-06-03 17:46:31 +0700

    fix router

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const router = require('./routes/index')
+const rootDir = require('./helpers/path')
 
 const app = express()
 app.set('view engine', 'ejs');
@@ -8,8 +10,11 @@ app.set('views', 'views')
 app.use(bodyParser.urlencoded())
 app.use(router)
 app.use(express.static('public'))
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+// app.use((req, res, next) => {
+//     res.status(404).send('<h1>Page Not Found</h1>')
+// })
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'index.html'))
 })
 
 app.listen(3000, () => {

fcd93725b4e868d49d7743d8552d3113b0883c0c
AnggaChelsea <v-angga.lesmana@anteraja.id>
2023-06-03 16:15:24 +0700

    add router

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
 const router = express.Router()
+const rootDir = require('../helpers/path')
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../', 'views', 'index.html'))
+    res.sendFile(path.join(rootDir, 'views', 'index.html'))
 })
+
+router.post('/contact-us', (req, res, next) => {})
 
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,5 @@ const adminRouter = require('./admin')
 const homeRouter = require('./home')
 
 router.use('/admin', adminRouter)
-router.use('/home', homeRouter)
 
 module.exports = router

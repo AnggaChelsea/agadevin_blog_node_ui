aaadbef9f55d6d6b3d0f0237f6c7f6b61a0cc001
AnggaChelsea <v-angga.lesmana@anteraja.id>
2023-06-08 08:43:55 +0700

    modified layouting

--- controllers/admin/admin_users_controller.js
+++ controllers/admin/admin_users_controller.js
@@ -1,12 +1,16 @@
 const subcribeModel = require('../../models/subscribe_model')
+const moment = require('moment')
 
 class AdminUsersList{
     static async getSubsriber(req, res, next){
         try{
             const admin = await subcribeModel.find()
             console.log(admin, 'admin')
+            admin.map(e => e.createdAt = moment.format(admin.createdAt))
             if(admin){
-                res.render('admin/admin')
+                res.render('admin/admin', {
+                    adminData: admin
+                })
             }else{
                 res.status(400).render('<h4>Maaf error 400</h4>')
             }
